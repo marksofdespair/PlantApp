bcbc3a223249f6c1de3668c9c1c298acaa53bd30
Dia <142447820+marksofdespair@users.noreply.github.com>
2024-01-04 18:48:12 -0600

    rollback Script.js

--- script.js
+++ script.js
@@ -11,13 +11,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     // Hide the "Add a New Plant" section initially
     addPlantSection.style.display = 'none';
-
-    // Check if the user's name is stored in local storage and display it
-    const storedName = localStorage.getItem('localName');
-    if (storedName) {
-        nameInput.value = storedName;
-        outputMessage.textContent = `Hello, ${storedName}`;
-    }
+    plantListSection.style.display= 'none';
 
     addPlantButton.addEventListener('click', function () {
         // This code runs when the "Add a Plant" button is clicked
@@ -26,6 +20,16 @@ window.addEventListener('DOMContentLoaded', function () {
             addPlantSection.style.display = 'block';
         } else {
             addPlantSection.style.display = 'none';
+        }
+    });
+
+    showPlantListButton.addEventListener('click', function () {
+        // This code runs when the "Show Plant List" button is clicked
+        // Toggle the visibility of the "Plant List" section
+        if (plantListSection.style.display === 'none') {
+            plantListSection.style.display = 'block';
+        } else {
+            plantListSection.style.display = 'none';
         }
     });
 
@@ -43,7 +47,7 @@ window.addEventListener('DOMContentLoaded', function () {
         } else if (month >= 6 && month <= 10) {
             return 'It\'s summer- consider checking the soil moisture twice a week!';
         } else {
-            return 'It\'s winter- consider checking the soil moisture every other week!';
+            return 'It\'s winter: consider checking the soil moisture every other week!';
         }
     }
 
@@ -60,10 +64,8 @@ window.addEventListener('DOMContentLoaded', function () {
         event.preventDefault();
         const name = nameInput.value;
         outputMessage.textContent = `Hello, ${name}`;
-
-        // Save the user's name to local storage
-        localStorage.setItem('localName', name);
     });
 
     updateMonthAndWaterRequirements();
 });
+
